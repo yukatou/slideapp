7fcd3e6f5639271027ddcb2f1e5903a8ee9d5334
yukatou <yu.lets0226@gmail.com>
2012-08-22 00:49:14 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slideapp::Application.config.secret_token = '37869b55fab7dd6359cf838b0507d428f89d2f14a2bf2370ba19b683a9bdf285cc9cadf37e644fdd34f3441d93dc3b176c9070c2564a5712936b7917e269949f'
