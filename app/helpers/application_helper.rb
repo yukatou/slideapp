edf415e9e7ce0b33a13900f182444c42312c6215
Fumiya KARASAWA <krswfmy@gmail.com>
2012-08-26 13:22:13 +0900

    truncate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def search_slide
     @search = Slide.search(params[:search])
   end
+  def truncate_lines(string, length)
+    count = 0
+    string.split("").each_with_index do |s,index|
+      break if count == 24
+      if s.bytesize == 1
+        count += 1
+      elsif s.bytesize == 3
+        length -= 1
+        count +=2
+      end
+    end
+    truncate(string, :length => length)
+  end
 end
